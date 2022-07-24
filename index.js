9b7ab69d56e55922906b1a01e44af485956e5f5a
Daryl Norrell <cheyannewascom@gmail.com>
2022-07-24 15:49:37 -0700

    commit tree 2b9a2d6d438d7b1fb5b0093b7409d70f5625233e

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+    name: "Daryl",
+    streetAddress: "117 colorad st",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   const newObj = {...employee };
+   
+   newObj[key] = value;
+
+   return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+    
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+
+    delete newEmployee["name"];
+    
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey() {
+    delete employee["name"];
+    return employee;
+}
+
